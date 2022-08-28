da04d6367f12bcf42773b2fa549bf67c3c1d1d5f
HedrisTemmyTop <hedristemitope2001@gmail.com>
2022-08-28 15:28:14 +0100

    second commit

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,9 +1,6 @@
-import { auth } from "./auth";
-
 export {
   addIngredient,
   removeIngredient,
   initIngredients,
 } from "./burgerBuilder";
 export {} from "./order";
-export { auth } from "./auth";
